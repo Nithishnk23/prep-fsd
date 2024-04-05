5f52844bffb6b3a56402d82f75fa0612485e6415
Nithishnk23 <knithish389@gmail.com>
2024-04-05 09:21:41 +0530

    First React Application

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+    <Greet/>
+    </div>
+  );
+}
+
+function Greet() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
+  );
+}
+export default App;
